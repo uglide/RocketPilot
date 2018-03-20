1dd0bb4e6d47f31377e20be114653af1082b3675
Igor Malinovskiy <u.glide@gmail.com>
2018-03-20 12:47:26 +0200

    Fix resource leaks

--- rocketpilot/application.py
+++ rocketpilot/application.py
@@ -3,11 +3,14 @@ from pykeyboard import PyKeyboard
 from rocketpilot.introspection import ProxyBase
 from rocketpilot.input import Mouse
 
+mouse_obj = Mouse()
+keyboard_obj = PyKeyboard()
+
 
 class ApplicationProxy(ProxyBase):
 
     def __init__(self, state_dict, path, backend):
         super().__init__(state_dict, path, backend)
 
-        self.mouse = Mouse()
-        self.keyboard = PyKeyboard()
+        self.mouse = mouse_obj
+        self.keyboard = keyboard_obj
